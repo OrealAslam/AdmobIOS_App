452fcd02d732cd5b6a24fec6dbc4b352371b63c3
PlanCare <apple@Hamzas-MacBook-Pro.local>
2024-11-15 13:24:19 +0500

    Splash Hiding Issue Fixed

--- ios/RNAdmob/AppDelegate.mm
+++ ios/RNAdmob/AppDelegate.mm
@@ -1,8 +1,19 @@
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"  // here
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h> // Import Linking Manager
 
 @implementation AppDelegate
+
+// - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+// {
+//   self.moduleName = @"RNAdmob";
+//   // You can add your custom initial props in the dictionary below.
+//   // They will be passed down to the ViewController used by React Native.
+//   self.initialProps = @{};
+
+//   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+// }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -10,9 +21,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  bool didFinish=[super application:application didFinishLaunchingWithOptions:launchOptions]; // added 
+  [RNSplashScreen show];  // here
+  return didFinish; // added 
 }
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
