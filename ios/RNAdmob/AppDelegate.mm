105e10474c37fe9495d39d67f70b251b4173e127
PlanCare <apple@Hamzas-MacBook-Pro.local>
2024-11-26 16:12:48 +0500

    App Name Updated

--- ios/RNAdmob/AppDelegate.mm
+++ ios/RNAdmob/AppDelegate.mm
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"RNAdmob";
+  self.moduleName = @"PulseTracker";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
